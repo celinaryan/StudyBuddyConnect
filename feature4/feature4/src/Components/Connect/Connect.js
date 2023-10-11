8ced0397d03a86942e9bf079ff7de8dc35697b98
Sofia Dillhoff <97558551+sdillho2@users.noreply.github.com>
2023-10-11 16:51:07 -0400

    adding comments

--- feature4/feature4/src/Components/Connect/Connect.js
+++ feature4/feature4/src/Components/Connect/Connect.js
@@ -1,4 +1,5 @@
 export default function Connect() {
+  // on this page students can connect and chat with other users who they matched with
     return (
       <section>
         <div className="centerText">
--- feature4/feature4/src/Components/YourMatches/YourMatches.js
+++ feature4/feature4/src/Components/YourMatches/YourMatches.js
@@ -1,4 +1,8 @@
 export default function YourMatches() {
+  // in this page, users can see who they matched with
+  // weather that be someone who needs help in the same course as them
+  // someone who can help them in a specific course
+  // someone they can help 
     return (
       <section>
         <div className="centerText">
