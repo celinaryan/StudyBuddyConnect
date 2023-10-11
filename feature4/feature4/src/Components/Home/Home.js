29ac9dddfed346b17016f1cd6a66eeb358a52f71
Sofia Dillhoff <97558551+sdillho2@users.noreply.github.com>
2023-10-11 14:09:06 -0400

    attemping to add image but idk why it wont load

--- feature4/feature4/src/Components/Home/Home.js
+++ feature4/feature4/src/Components/Home/Home.js
@@ -4,6 +4,7 @@ export default function Home() {
         <h1> Welcome to Study Buddy Connect! </h1>
         <p> Empowering college students through collaborative learning and connections
       for academic success. </p>
+        <img src="images/studentsStudying.jpeg" alt="students studying" width="400" height="225" className="center" />
       </section>
     );
   }
