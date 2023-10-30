7db285679baf5ee82eba803b22c89f2cb9a4c2ed
Sofia Dillhoff <97558551+sdillho2@users.noreply.github.com>
2023-10-30 19:51:40 -0400

    starting to figure out how to add ProtectedRoute

---
+++ feature4/feature4/src/Components/Auth/AuthService.js
@@ -0,0 +1,48 @@
+import Parse from "parse";
+// only include parse in here and in app.js
+// used in auth register component
+export const createUser = (newUser) => {
+  const user = new Parse.User();
+
+  user.set("username", newUser.email);
+  user.set("firstName", newUser.firstName);
+  user.set("lastName", newUser.lastName);
+  user.set("password", newUser.password);
+  user.set("email", newUser.email);
+
+  console.log("User: ", user);
+  return user
+    .signUp()
+    .then((newUserSaved) => {
+      return newUserSaved;
+    })
+    .catch((error) => {
+      alert(`Error: ${error.message}`);
+    });
+};
+
+// used in auth login component
+// we still set password and username but we pull them (email and password) off and pass those in as arguments for the user.login method
+export const loginUser = (currUser) => {
+  const user = new Parse.User();
+
+  user.set("password", currUser.password);
+  user.set("username", currUser.email);
+
+  console.log("User: ", user);
+  console.log();
+  return user
+    .logIn(user.email, user.password)
+    .then((currUserSaved) => {
+      return currUserSaved;
+    })
+    .catch((error) => {
+      alert(`Error: ${error.message}`);
+    });
+};
+
+// ES6 shorthand - a way to do defensive coding
+// if parse.user.current() && parse.user.current().authenticated
+export const checkUser = () => {
+  return Parse.User.current()?.authenticated;
+};
