183ef1e915a92f2a7a691a7aeeaeebfaa8ff369d
celinaryan <cryan27@nd.edu>
2023-11-29 18:02:03 -0500

    css jaunt

--- feature4/feature4/src/Components/YourMatches/MatchCard.js
+++ feature4/feature4/src/Components/YourMatches/MatchCard.js
@@ -3,12 +3,14 @@ const MatchCard = ({ user, canHelpClass, needHelpClass }) => {
     if (!user) return <div>Error: User information is not available.</div>;
     return (
         <div className="match-card">
-        <div className="match-card-info">
+        <div className="match-info-container">
           <h3 className="match-card-header">User: {user.name} {user.lastname}</h3>
           {canHelpClass && <h3 className="match-card-class">Can help with class: {canHelpClass.name}</h3>}
           {needHelpClass && <h3 className="match-card-class">Needs help with class: {needHelpClass.name}</h3>}
         </div>
-        <button className="match-card-button">Chat</button>
+        <div className="button-container">
+          <button className="match-card-button">Start Chatting</button>
+        </div>
       </div>
     );
   };
